8973d078842fb4b578696a7c339100cc203e69af
undertrox <undertrox@gmail.com>
2018-01-23 10:55:19 +0100

    Add html and css file

--- map.js
+++ map.js
@@ -1,28 +1,28 @@
 function initMap() {
-    var uluru = {lat: -25.363, lng: 131.044};
+    /*
+    var buildings = {
+        nBuilding: {
+            pos: {lat: 51.968522, lng: 12.091052},
+            descriptionFile: "desc/nBuilding.html"
+        }
+    };
+    */
+    // TODO: alle Objekte in buildings als Marker zur Map hinzufügen
+    // TODO: für jedes Objekt in buildings per Ajax die Beschreibung laden
+
+    var nBuilding = {lat: 51.968522, lng: 12.091052};
     var map = new google.maps.Map(document.getElementById('map'), {
-        zoom: 4,
-        center: uluru
+        zoom: 20,
+        mapTypeId: 'satellite',
+        center: nBuilding
     });
 
     var contentString = '<div id="content">'+
         '<div id="siteNotice">'+
         '</div>'+
-        '<h1 id="firstHeading" class="firstHeading">Uluru</h1>'+
+        '<h1 id="firstHeading" class="firstHeading">N-Gebäude</h1>'+
         '<div id="bodyContent">'+
-        '<p><b>Uluru</b>, also referred to as <b>Ayers Rock</b>, is a large ' +
-        'sandstone rock formation in the southern part of the '+
-        'Northern Territory, central Australia. It lies 335&#160;km (208&#160;mi) '+
-        'south west of the nearest large town, Alice Springs; 450&#160;km '+
-        '(280&#160;mi) by road. Kata Tjuta and Uluru are the two major '+
-        'features of the Uluru - Kata Tjuta National Park. Uluru is '+
-        'sacred to the Pitjantjatjara and Yankunytjatjara, the '+
-        'Aboriginal people of the area. It has many springs, waterholes, '+
-        'rock caves and ancient paintings. Uluru is listed as a World '+
-        'Heritage Site.</p>'+
-        '<p>Attribution: Uluru, <a href="https://en.wikipedia.org/w/index.php?title=Uluru&oldid=297882194">'+
-        'https://en.wikipedia.org/w/index.php?title=Uluru</a> '+
-        '(last visited June 22, 2009).</p>'+
+        '<p>Text....</p>'+
         '</div>'+
         '</div>';
 
@@ -31,9 +31,9 @@ function initMap() {
     });
 
     var marker = new google.maps.Marker({
-        position: uluru,
+        position: nBuilding,
         map: map,
-        title: 'Uluru (Ayers Rock)'
+        title: 'Francisceum Zerbst'
     });
     marker.addListener('click', function() {
         infowindow.open(map, marker);
