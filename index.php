a6f08ebb8cdf246e2e1b7a89a9b040c91d8def53
undertrox <undertrox@gmail.com>
2018-03-15 20:28:33 +0100

    Really fixed redirect

--- index.php
+++ index.php
@@ -1,4 +1,10 @@
-<?php
-        header('Location: https://history-overview.herokuapp.com/gm/index.html');
-        die();
->
+<html>
+<head>
+    <script language="javascript" type="text/javascript">
+        <!--
+        window.open("https://history-overview.herokuapp.com/gm/")
+        // -->
+    </script>
+</head>
+<body></body>
+</html>
